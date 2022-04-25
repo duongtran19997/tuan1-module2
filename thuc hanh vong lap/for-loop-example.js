67ca8f83a25f98e0fa2b146e458f40246cc2b369
superman <duonga2qp@gmail.com>
2022-04-25 21:46:19 +0700

    nopbai

---
+++ thuc hanh vong lap/for-loop-example.js
@@ -0,0 +1,16 @@
+function geek(){
+    let obj = {name: 'Peter', age: 25, salary: 3000};
+    console.log('for(;;)')
+    for (let i =0; i<=10;i+=2){
+        console.log(i+'')
+    }
+    console.log('for...of')
+    for (let i of ['Ha Noi',20.32,2000]){
+        console.log(i+'')
+    }
+    console.log('for...in')
+    for (const i in obj) {
+        console.log(obj[i]+'')
+    }
+}
+geek()
